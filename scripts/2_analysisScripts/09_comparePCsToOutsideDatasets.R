d36011fa2ee74e80a03e44e0167307b57357a64d
hcnapier <hailey.napier@duke.edu>
2025-10-02 14:35:05 -0400

    setup hao pc pseudobulk

--- scripts/2_analysisScripts/09_comparePCsToOutsideDatasets.R
+++ scripts/2_analysisScripts/09_comparePCsToOutsideDatasets.R
@@ -16,11 +16,32 @@ pseudobulkMerged %>%
   as.data.frame() %>%
   select(contains("Purkinje")) -> pseudobulkMerged_pcs
 names(pseudobulkMerged_pcs) <- paste(names(pseudobulkMerged_pcs), "Napier", sep = "_")
+pseudobulkMerged_pcs <- pseudobulkMerged_pcs[rowSums(pseudobulkMerged_pcs[, -1])>0, ]
+geneList <- pseudobulkMerged_pcs %>% rownames()
 rm(pseudobulkMerged)
-rm(temp)
 
 ## 0.3 Setup Hao rhesus data ----
+# load data 
 setwd("~/Work/VertGenLab/Projects/zebrinEvolution/Code/haoReanalysis/data/seuratObjs")
 haoRhesus <- readRDS("macaqueSingleCellPC.rds")
+# aggregate expression
+pseudobulk_haoRhesus <- AggregateExpression(haoRhesus, features = geneList, group.by = "doublet_info", return.seurat = F, assays = "RNA") #group by doublet_info to lump all cells together
+# convert to dense matrix 
+pseudobulk_haoRhesus <- pseudobulk_haoRhesus$RNA %>% as.matrix()
+pseudobulk_haoRhesus <- pseudobulk_haoRhesus[rowSums(pseudobulk_haoRhesus)>0, ] 
+rm(haoRhesus)
 
 ## 0.4 Setup Hao marmoset data ----
+# load data 
+setwd("~/Work/VertGenLab/Projects/zebrinEvolution/Code/haoReanalysis/data/seuratObjs")
+haoMarmoset <- readRDS("marmosetSingleCellPC.rds")
+# aggregate expression
+pseudobulk_haoMarmoset <- AggregateExpression(haoMarmoset, features = geneList, group.by = "doublet_info", return.seurat = F, assays = "RNA") #group by doublet_info to lump all cells together
+# convert to dense matrix 
+pseudobulk_haoMarmoset <- pseudobulk_haoMarmoset$RNA %>% as.matrix()
+pseudobulk_haoMarmoset <- pseudobulk_haoMarmoset[rowSums(pseudobulk_haoMarmoset)>0, ] 
+rm(haoMarmoset)
+
+## 0.5 Setup Bartelt mouse data ----
+# load data 
+
