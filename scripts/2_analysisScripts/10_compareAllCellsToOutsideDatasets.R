510ad93f5898f1f26e5f71fe570f3bb87147d4e9
hcnapier <napier1@kenyon.edu>
2025-10-03 16:38:55 -0400

    begin across cell type comparisons

---
+++ scripts/2_analysisScripts/10_compareAllCellsToOutsideDatasets.R
@@ -0,0 +1,40 @@
+# 10_compareToOutsideDatasets.R
+# Compare Primate Pilot dataset transcriptional similarity to other datasets 
+# October 3, 2025
+# Hailey Napier
+
+# 0.0 Setup ----
+## 0.1 Load packages ----
+require(Seurat)
+require(dplyr)
+require(ggplot2)
+require(reshape2)
+
+## 0.2 Source functions ----
+setwd("~/Work/VertGenLab/Projects/zebrinEvolution/Code/primatePilot/functions")
+source("pseudobulkHumanOrtholog.R")
+
+## 0.3 Setup one to one orthologs ----
+setwd("/Users/haileynapier/Work/VertGenLab/Projects/zebrinEvolution/Data/geneLists/orthologs")
+allPrimateOrthologs <- read.delim("human_MouseMarmosetMacaque_orthologs.txt", sep = "\t", header = T)
+names(allPrimateOrthologs) <- tolower(names(allPrimateOrthologs))
+allPrimateOrthologs %>%
+  filter(macaque.homology.type == "ortholog_one2one") %>%
+  filter(mouse.homology.type == "ortholog_one2one") %>%
+  filter(white.tufted.ear.marmoset.homology.type == "ortholog_one2one") %>%
+  distinct() -> allPrimateOrthologs
+
+## 0.4 Setup primate pilot data ----
+setwd("~/Work/VertGenLab/Projects/zebrinEvolution/Code/primatePilot/data/")
+pseudobulkMerged <- readRDS("pseudobulkMerged.rds")
+pseudobulkMerged %>% 
+  as.data.frame() %>%
+  select(contains("Purkinje")) -> pseudobulkMerged_pcs
+names(pseudobulkMerged_pcs) <- paste(names(pseudobulkMerged_pcs), "Napier", sep = "_")
+pseudobulkMerged_pcs <- pseudobulkMerged_pcs[rowSums(pseudobulkMerged_pcs[, -1])>0, ]
+rm(pseudobulkMerged)
+pseudobulkMerged_pcs$gene.name <- rownames(pseudobulkMerged_pcs)
+
+## 0.5 Setup Hao rhesus data ----
+pseudobulkHumanOrtholog()
+
