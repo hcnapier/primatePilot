157d9dcfe6e14d38a739762f3b03e74ebf626e59
hcnapier <napier1@kenyon.edu>
2025-10-29 16:16:35 -0400

    Re-normalize using shifted logarithm with L = meanReadDepth

---
+++ functions/shiftedLogNorm.R
@@ -0,0 +1,30 @@
+# shiftedLogNorm
+# Function to normalize a count matrix using the shifted log method 
+# See https://www.sc-best-practices.org/preprocessing_visualization/normalization.html#shifted-logarithm
+# October 29, 2025
+
+require(tibble)
+require(dplyr)
+
+source("~/Work/VertGenLab/Projects/zebrinEvolution/Code/primatePilot/functions/removeZeroRowsCols.R")
+
+shiftedLogNorm <- function(countMat, pseudocount = 1, useForL = "meanReadDepth"){
+  if(!is.matrix(countMat)){
+    stop("countMat must by of type matrix")
+  }
+  s <- countMat
+  s[] <- 0
+  for(currCell in colnames(countMat)){ # Generate a matrix containing the size factor numerator (read depth per cell)
+    s[,currCell] <- countMat[,currCell] %>% sum()
+  }
+  if(useForL == "meanReadDepth"){
+    L = countMat %>% colSums() %>% mean()
+  }else if(is.numeric(useForL)){
+    L = useForL
+  }else{
+    stop("Please input a valid option to useForL")
+  }
+  s = s/L
+  scaledMat = log((countMat/s) + pseudocount)
+  return(scaledMat)
+}
