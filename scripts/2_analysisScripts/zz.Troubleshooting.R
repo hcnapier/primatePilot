cdd7ac0d988f8aebaaa1b4438f1c7752495b0b96
hcnapier <napier1@kenyon.edu>
2025-11-04 12:04:08 -0500

    troubleshoot negative binomial

--- scripts/2_analysisScripts/zz.Troubleshooting.R
+++ scripts/2_analysisScripts/zz.Troubleshooting.R
@@ -1,18 +1,42 @@
-speciesNames <- c("human", "rhesus", "mouse")
-rawList_hto <- list()
-setwd("/Users/haileynapier/Work/VertGenLab/Projects/zebrinEvolution/Data/20250423_PrimatePilot/cellrangerOuts")
-for(currSpecies in speciesNames){
-  filename <- paste(currSpecies, "raw_feature_bc_matrix.h5", sep = "_")
-  print(filename)
-  tmp <- Read10X_h5(filename)
-  #rawList[[currSpecies]] <- tmp$`Gene Expression`
-  rawList_hto[[currSpecies]] <- tmp$`Antibody Capture`
-  rm(tmp)
-}
+testNBin <- rnbinom(100, size = 10, mu = 8)
+hist(testNBin, breaks = 10)
+z <- mean(testNBin)
+num <- sum(testNBin)
+s <- num/z
+testNBin_norm <- log((testNBin/s) + 1)
+hist(testNBin_norm)
+var(testNBin)
+var(testNBin_norm)
 
-rawList_hto[["human"]] %>% sum()
-rawList_hto[["mouse"]] %>% sum()
-rawList_hto[["rhesus"]] %>% sum()
+z2 <- 14
+s2 <- num/z2
+testNBin_norm2 <- log((testNBin/s2)+1)
+hist(testNBin_norm2, breaks = 10)
+var(testNBin_norm2)
 
-rawList_hto[["human"]] %>% removeZeroRowsCols()
-rawList_hto[["rhesus"]] %>% removeZeroRowsCols()
+var(testNBin_norm)/mean(testNBin_norm)
+var(testNBin_norm2)/mean(testNBin_norm2)
+sd(testNBin_norm)/mean(testNBin_norm)
+sd(testNBin_norm2)/mean(testNBin_norm2)
+
+testNBin2 <- rnbinom(200, size = 2, mu = 2)
+hist(testNBin2)
+z_dist2 <- mean(testNBin2)
+num_dist2 <- sum(testNBin2)
+s_dist2 <- num_dist2/z_dist2
+testNBin2_norm <- log(testNBin/s_dist2 + 1)
+hist(testNBin2_norm)
+var(testNBin2)
+var(testNBin2_norm)
+
+testNBin2_norm2 <- log(testNBin2/s + 1)
+hist(testNBin2_norm2)
+var(testNBin2_norm2)
+
+z_avg <- mean(c(testNBin2, testNBin))
+num_avg <- sum(testNBin, testNBin2)
+s_avg <- num_avg/z_avg
+testNBin2_normAvg <- log(testNBin2/s_avg + 1)
+testNBin_normAvg <- log(testNBin/s_avg + 1)
+hist(testNBin2_normAvg)
+hist(testNBin_normAvg)
